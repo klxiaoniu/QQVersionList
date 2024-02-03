0314a87de39aabf0d77ce7f52a6588c2d1eb0969
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-02-03 20:23:04 +0800

    Revert "更改布局，更改进度对话框，启用动态配色，更新界面UI、空格猜版、桌面图标 (#1)"
    This reverts commit a00947f48f7efae060c589dda69e2886a5f3e1e4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,6 @@ android {
     namespace = "com.xiaoniu.qqversionlist"
     compileSdk = 34
 
-//    buildFeatures {
-//        compose = true
-//    }
-//
-//    composeOptions {
-//        kotlinCompilerExtensionVersion = "1.5.8"
-//    }
-
-
     defaultConfig {
         applicationId = "com.xiaoniu.qqversionlist"
         minSdk = 24
@@ -60,46 +51,4 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
-
-    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
-    implementation("androidx.compose:compose-bom:2024.01.00")
-    androidTestImplementation("androidx.compose:compose-bom:2024.01.00")
-
-//    // Choose one of the following:
-//    // Material Design 3
-//    implementation("androidx.compose.material3:material3")
-//    // or Material Design 2
-//    implementation("androidx.compose.material:material")
-//    // or skip Material Design and build directly on top of foundational components
-//    implementation("androidx.compose.foundation:foundation")
-//    // or only import the main APIs for the underlying toolkit systems,
-//    // such as input and measurement/layout
-//    implementation("androidx.compose.ui:ui")
-//
-//    // Android Studio Preview support
-//    implementation("androidx.compose.ui:ui-tooling-preview")
-//    debugImplementation("androidx.compose.ui:ui-tooling")
-//
-//    // UI Tests
-//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
-//
-//    // Optional - Included automatically by material, only add when you need
-//    // the icons but not the material library (e.g. when using Material3 or a
-//    // custom design system based on Foundation)
-//    implementation("androidx.compose.material:material-icons-core")
-//    // Optional - Add full set of material icons
-//    implementation("androidx.compose.material:material-icons-extended")
-//    // Optional - Add window size utils
-//    implementation("androidx.compose.material3:material3-window-size-class")
-//
-//    // Optional - Integration with activities
-//    implementation("androidx.activity:activity-compose:1.8.2")
-//    // Optional - Integration with ViewModels
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-//    // Optional - Integration with LiveData
-//    implementation("androidx.compose.runtime:runtime-livedata")
-//    // Optional - Integration with RxJava
-//    implementation("androidx.compose.runtime:runtime-rxjava2")
-
 }
