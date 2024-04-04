9496a70a5e69ce7a0b6ece2c3f14153235cefff7
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-04-05 07:51:13 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0-beta01")
+    implementation("com.google.android.material:material:1.12.0-rc01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-common:8.3.1")
     testImplementation("junit:junit:4.13.2")
