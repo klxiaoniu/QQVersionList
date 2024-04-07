1ae0d9468c3533a85c0d5fdaacf9889a5bc1a24e
ArcticFox <139336664+ArcticFoxPro@users.noreply.github.com>
2024-04-07 11:26:38 +0800

    修复部分机型桌面图标边缘出现黑线的问题、修复部分机型展开版本列表卡片时文字跳动的问题、修复横竖屏切换时 Activity 被重载的问题、启动画面适配动态配色
    - 修复：部分机型（如 HUAWEI Mate 60 Pro、Xiaomi Pad 5）桌面图标边缘出现黑线的问题
    - 修复：部分机型（如 HUAWEI Mate 60 Pro）展开版本列表卡片时文字跳动的问题
    - 修复：横竖屏切换时 Activity 被重载的问题
    - 优化：启动画面适配动态配色

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0-rc01")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("com.google.android.material:material:1.12.0-rc01")
