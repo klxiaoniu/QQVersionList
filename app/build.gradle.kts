eb8cae2251078613fb8b44a88f138c2c6c0907b7
ArcticFoxPro <139336664+ArcticFoxPro@users.noreply.github.com>
2024-04-20 00:41:56 +0800

    SplashScreen 上游更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.0-rc01")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-splashscreen:1.1.0-rc01")
     implementation("com.google.android.material:material:1.12.0-rc01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
