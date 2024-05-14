1bfc49d947bf92bf446c86853ea5a0749f3f9f3e
ArcticFoxPro <139336664+ArcticFoxPro@users.noreply.github.com>
2024-05-14 13:55:02 +0800

    提升 JVM 版本至 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     buildFeatures {
         viewBinding = true
