d6af64392f7c8d1f20285939ceea14ac43d535be
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-08-31 13:13:33 +0800

    ```更新Kotlin插件和库版本以提升性能和兼容性
    升级Kotlin插件和库版本，包括在build.gradle.kts和.app/build.gradle.kts文件中从2.0.0更新至2.0.20，以利用最新版本的功能和改进。版本更新涉及Kotlin插件.serialization和org.jetbrains.kotlin.android插件。
    ```

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ import java.io.ByteArrayOutputStream
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.20"
 }
 
 fun String.execute(currentWorkingDir: File = file("./")): String {
