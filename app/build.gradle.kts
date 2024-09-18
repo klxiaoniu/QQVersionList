84f74baf0ea3b581dfdd1c850d08bbe97cef5111
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-09-18 20:10:49 +0800

    refactor(layouts): 将LinearLayout替换为MaterialButtonGroup
    多处布局文件中的LinearLayout已被MaterialButtonGroup替换，以改进UI一致性并优化按钮布局。更新后的布局提供了更好的视觉效果和功能性。
    
    涉及的文件包括bottomsheet_shiply_advanced_config.xml、dialog_format_define.xml、dialog_guess.xml、dialog_loading.xml、dialog_shiply.xml、shiply_link_next_button.xml、success_button.xml和update_qvt_button.xml。
    
    依赖项提升(material-library): 升级到1.13.0-alpha06
    
    Google Material Library已更新至1.13.0-alpha06版本，以支持新引入的MaterialButtonGroup和其他功能。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-splashscreen:1.1.0-rc01")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0-alpha06")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
