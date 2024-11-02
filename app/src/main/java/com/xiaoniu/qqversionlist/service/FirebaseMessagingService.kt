bf9168b49970306b61bcc2532770e5bfd61addea
有鲫雪狐 <139336664+ArcticFoxPro@users.noreply.github.com>
2024-11-03 01:42:25 +0800

    refactor: 更新开源许可证标题并添加代码注释
    - 在多个语言的 strings.xml 文件中，将"开源相关"修改为"开放源代码许可"
    - 在 FirebaseMessagingService.kt 文件中添加版权信息和 AGPLv3 许可证注释

--- app/src/main/java/com/xiaoniu/qqversionlist/service/FirebaseMessagingService.kt
+++ app/src/main/java/com/xiaoniu/qqversionlist/service/FirebaseMessagingService.kt
@@ -1,8 +1,25 @@
+/*
+    QQ Versions Tool for Android™
+    Copyright (C) 2023 klxiaoniu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 package com.xiaoniu.qqversionlist.service
 
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
